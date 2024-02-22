12146343c7050e8365e0dcd844c150c2ad4781a7
Josh ODell <ksinc.josh@gmail.com>
2024-02-21 23:06:28 -0600

    tip calc update and readme update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 I have coded with various languages over the years and never had to use it on a daily basis. This repo is for my various python projects and is the result of me going back to "relearn" python.
+
+Although these will mostly be simple learning projects, I want to keep them here so I can "buff" them later. As an example, the tip calculator is my first entry into this repo. It is only a few lines of code but I still want it here to potentially update it later into something more robust as my skills improve and my collection of projects grows.
--- TIP_CALCULATOR/tip_calc.py
+++ TIP_CALCULATOR/tip_calc.py
@@ -1,7 +1,9 @@
 print("Welcome to the tip calculator.")
-totalBill = float((input("What was the total bill? $")))
+bill = float((input("What was the total bill? $")))
 partySize = int(input("How many people to split the bill? "))
 percentTip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-perPersonPay = round((totalBill / partySize) * (percentTip / 100 + 1), 2)
+totalBill = round(bill * (percentTip / 100 + 1), 2)
+perPersonPay = round(totalBill/ partySize, 2)
 
-print("Each person should pay: $" + str(perPersonPay))
+print(f"Total bill: ${totalBill}")
+print(f"Each person should pay: ${perPersonPay}")
