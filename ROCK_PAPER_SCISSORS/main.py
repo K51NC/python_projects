ba512732877e4162f4dd36e761ddb108f2203bc3
Josh ODell <ksinc.josh@gmail.com>
2024-02-22 09:44:43 -0600

    updated art for RPS

--- ROCK_PAPER_SCISSORS/main.py
+++ ROCK_PAPER_SCISSORS/main.py
@@ -1,8 +1,10 @@
 import random
+import art
 
 print("Welcome to RPS!")
 
-userPick = input("Select (r)ock, (p)aper, or (s)cissors: ")
+userPick = input("Select (r)ock, (p)aper, or (s)cissors: ").lower()
+userHand = "User hand did not get assigned."
 random_integer = random.randint(0, 2)
 compPick = -1
 
@@ -15,16 +17,34 @@ elif random_integer == 2:
 else:
     print("There was an error with the computer picking.")
 
-print(f"You picked {userPick}.")
-print(f"The computer picked {compPick}.")
+if userPick == "r":
+    userHand = art.rock
+elif userPick == "p":
+    userHand = art.paper
+elif userPick == "s":
+    userHand = art.scissors
+else:
+    print("Something went wrong when assigning art to the user's hand.")
+
+if compPick == "r":
+    compHand = art.rock
+elif compPick == "p":
+    compHand = art.paper
+elif compPick == "s":
+    compHand = art.scissors
+else:
+    print("Something went wrong when assigning art to the computer's hand.")
+
+print(f"You picked:\n{userHand}")
+print(f"The computer picked: {compHand}.")
 
 if userPick == "r":
     if compPick == "r":
         print("Draw!")
     elif compPick == "p":
-        print("You win!")
-    elif compPick == "s":
         print("The computer wins!")
+    elif compPick == "s":
+        print("You win!")
     else:
         print("Something went wrong after you picked 'r'.")
 elif userPick == "p":
